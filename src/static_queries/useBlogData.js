2b9a7958e0630139c61046ecaeabf663b018835c
a-polito <leonida987@hotmail.it>
2022-12-29 00:28:10 +0100

    feat: managing infinite scrolling

--- src/static_queries/useBlogData.js
+++ src/static_queries/useBlogData.js
@@ -1,3 +1,4 @@
+import React, {useState, useEffect} from "react"
 import { graphql, useStaticQuery } from "gatsby"
 
 export default function useBlogData() {
@@ -32,5 +33,46 @@ export default function useBlogData() {
       }
     }
   `)
-  return data.allMarkdownRemark.edges
+  
+  const allBlogPages = data.allMarkdownRemark.edges
+  
+  // State for the list
+  const [list, setList] = useState([...allBlogPages.slice(0, 10)])
+
+  // State to trigger oad more
+  const [loadMore, setLoadMore] = useState(false)
+
+  // State of whether there is more to load
+  const [hasMore, setHasMore] = useState(allBlogPages.length > 10)
+
+  // Load more button click
+  const handleLoadMore = () => {
+    setLoadMore(true)
+  }
+
+  // Handle loading more articles
+  useEffect(() => {
+    if (loadMore && hasMore) {
+      const currentLength = list.length
+      const isMore = currentLength < allBlogPages.length
+      const nextResults = isMore
+        ? allBlogPages.slice(currentLength, currentLength + 10)
+        : []
+      setList([...list, ...nextResults])
+      setLoadMore(false)
+    }
+  }, [loadMore, hasMore]) //eslint-disable-line
+
+  //Check if there is more
+  useEffect(() => {
+    const isMore = list.length < allBlogPages.length
+    setHasMore(isMore)
+  }, [list]) //eslint-disable-line
+
+  return {
+    allBlogPages,
+    pagedBlogPages: list,
+    hasMore,
+    handleLoadMore
+  }
 }
